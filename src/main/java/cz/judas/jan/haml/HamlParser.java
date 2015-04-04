12e6b65b663bcbac91f9aa1a61849b3031991c3f
Jan Judas <jan@judas.cz>
2015-04-04 14:41:35 +0200

    text content

--- src/main/java/cz/judas/jan/haml/HamlParser.java
+++ src/main/java/cz/judas/jan/haml/HamlParser.java
@@ -1,7 +1,5 @@
 package cz.judas.jan.haml;
 
-import org.apache.commons.lang.StringUtils;
-
 import java.util.ArrayDeque;
 import java.util.Deque;
 
@@ -14,9 +12,22 @@ public class HamlParser {
             if(line.equals("!!! 5")) {
                 stringBuilder.append("<!DOCTYPE html>\n");
             } else {
-                String strippedLine = StringUtils.stripStart(line, "\t");
-                stack.push(strippedLine);
-                stringBuilder.append('<').append(strippedLine).append(">");
+                int numTabs = 0;
+                while(line.charAt(numTabs) == '\t') {
+                    numTabs++;
+                }
+                String strippedLine = line.substring(numTabs);
+
+                int spaceIndex = strippedLine.indexOf(' ');
+                String tagName;
+                if(spaceIndex == -1) {
+                    tagName = strippedLine;
+                    stringBuilder.append('<').append(strippedLine).append(">");
+                } else {
+                    tagName = strippedLine.substring(0, spaceIndex);
+                    stringBuilder.append('<').append(tagName).append(">").append(strippedLine.substring(spaceIndex + 1));
+                }
+                stack.push(tagName);
             }
         }
 
--- src/test/java/cz/judas/jan/haml/HamlParserTest.java
+++ src/test/java/cz/judas/jan/haml/HamlParserTest.java
@@ -29,4 +29,9 @@ public class HamlParserTest {
     public void doctype() throws Exception {
         assertThat(parser.process("!!! 5"), is("<!DOCTYPE html>\n"));
     }
+
+    @Test
+    public void tagsCanHaveTextContent() throws Exception {
+        assertThat(parser.process("html\n\thead\n\t\ttitle something"), is("<html><head><title>something</title></head></html>"));
+    }
 }
