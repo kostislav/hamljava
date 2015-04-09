98015d4e5c4a30e831458ff61c0b857e78cb2055
Jan Judas <jan@judas.cz>
2015-04-09 20:10:43 +0200

    inline holder classes

--- src/main/java/cz/judas/jan/haml/tokens/DocumentToken.java
+++ src/main/java/cz/judas/jan/haml/tokens/DocumentToken.java
@@ -1,12 +1,27 @@
 package cz.judas.jan.haml.tokens;
 
+import com.google.common.collect.ImmutableList;
 import cz.judas.jan.haml.ParseException;
 import cz.judas.jan.haml.mutabletree.MutableRootNode;
 import cz.judas.jan.haml.tokens.generic.AnyNumberOfToken;
+import cz.judas.jan.haml.tokens.generic.AnyOfToken;
+import cz.judas.jan.haml.tokens.generic.AtMostOneToken;
+import cz.judas.jan.haml.tokens.generic.SequenceOfTokens;
 
 public class DocumentToken implements Token<MutableRootNode> {
     private final Token<MutableRootNode> lines = new AnyNumberOfToken<>(
-            new AnyLineToken()
+            new SequenceOfTokens<>(ImmutableList.of(
+                    new AnyOfToken<>(ImmutableList.of(
+                            new DoctypeToken(),
+                            new SequenceOfTokens<>(ImmutableList.of(
+                                    new SignificantWhitespaceToken(),
+                                    new HtmlTagToken()
+                            ))
+                    )),
+                    new AtMostOneToken<>(
+                            new NewLineToken<MutableRootNode>()
+                    )
+            ))
     );
 
     @Override
