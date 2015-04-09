e8b50eb92f009d7332e3eaac65235341ac8150dc
Jan Judas <jan@judas.cz>
2015-04-09 19:39:39 +0200

    LineToken

--- src/main/java/cz/judas/jan/haml/HamlParser.java
+++ src/main/java/cz/judas/jan/haml/HamlParser.java
@@ -1,25 +1,19 @@
 package cz.judas.jan.haml;
 
-import com.google.common.collect.ImmutableList;
 import cz.judas.jan.haml.mutabletree.MutableRootNode;
 import cz.judas.jan.haml.tokens.*;
 import cz.judas.jan.haml.tree.Node;
 
-import static cz.judas.jan.haml.tokens.SequenceOfTokens.sequence;
-
 public class HamlParser {
     private final Token<MutableRootNode> doctypeToken = new DoctypeToken();
-    private final Token<MutableRootNode> hyperToken = sequence(ImmutableList.of(
-            new SignificantWhitespaceToken(),
-            new HtmlTagToken()
-    ));
+    private final Token<MutableRootNode> lineToken = new LineToken();
 
     public String process(String haml) throws ParseException {
         MutableRootNode rootNode = new MutableRootNode();
 
         for (String line : haml.split("\n")) {
             if(doctypeToken.tryEat(line, 0, rootNode) == -1) {
-                hyperToken.tryEat(line, 0, rootNode);
+                lineToken.tryEat(line, 0, rootNode);
             }
         }
 
---
+++ src/main/java/cz/judas/jan/haml/tokens/LineToken.java
@@ -0,0 +1,19 @@
+package cz.judas.jan.haml.tokens;
+
+import com.google.common.collect.ImmutableList;
+import cz.judas.jan.haml.ParseException;
+import cz.judas.jan.haml.mutabletree.MutableRootNode;
+
+import static cz.judas.jan.haml.tokens.SequenceOfTokens.sequence;
+
+public class LineToken implements Token<MutableRootNode> {
+    private final Token<MutableRootNode> tokens = sequence(ImmutableList.of(
+            new SignificantWhitespaceToken(),
+            new HtmlTagToken()
+    ));
+
+    @Override
+    public int tryEat(String line, int position, MutableRootNode parsingResult) throws ParseException {
+        return tokens.tryEat(line, position, parsingResult);
+    }
+}
