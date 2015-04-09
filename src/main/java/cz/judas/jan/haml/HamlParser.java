336f8b86af624f164f0dd3db1415c0420fcff4f9
Jan Judas <jan@judas.cz>
2015-04-09 19:31:54 +0200

    SignificantWhitespaceToken

--- src/main/java/cz/judas/jan/haml/HamlParser.java
+++ src/main/java/cz/judas/jan/haml/HamlParser.java
@@ -3,11 +3,13 @@ package cz.judas.jan.haml;
 import cz.judas.jan.haml.mutabletree.MutableRootNode;
 import cz.judas.jan.haml.tokens.DoctypeToken;
 import cz.judas.jan.haml.tokens.HtmlTagToken;
+import cz.judas.jan.haml.tokens.SignificantWhitespaceToken;
 import cz.judas.jan.haml.tokens.Token;
 import cz.judas.jan.haml.tree.Node;
 
 public class HamlParser {
     private final Token<MutableRootNode> doctypeToken = new DoctypeToken();
+    private final Token<MutableRootNode> whitespaceToken = new SignificantWhitespaceToken();
     private final Token<MutableRootNode> tagToken = new HtmlTagToken();
 
     public String process(String haml) throws ParseException {
@@ -15,13 +17,11 @@ public class HamlParser {
 
         for (String line : haml.split("\n")) {
             if(doctypeToken.tryEat(line, 0, rootNode) == -1) {
-                int numTabs = leadingTabs(line);
-
-                while (numTabs < rootNode.nestingLevel()) {
-                    rootNode.levelUp();
+                int newPosition = whitespaceToken.tryEat(line, 0, rootNode);
+                if(newPosition == -1) {
+                    newPosition = 0;
                 }
-
-                tagToken.tryEat(line, numTabs, rootNode);
+                tagToken.tryEat(line, newPosition, rootNode);
             }
         }
 
@@ -30,13 +30,5 @@ public class HamlParser {
         finalNode.appendTo(stringBuilder);
 
         return stringBuilder.toString();
-    }
-
-    private int leadingTabs(String line) {
-        int numTabs = 0;
-        while (line.charAt(numTabs) == '\t') {
-            numTabs++;
-        }
-        return numTabs;
     }
 }
---
+++ src/main/java/cz/judas/jan/haml/tokens/SignificantWhitespaceToken.java
@@ -0,0 +1,29 @@
+package cz.judas.jan.haml.tokens;
+
+import cz.judas.jan.haml.ParseException;
+import cz.judas.jan.haml.mutabletree.MutableRootNode;
+
+public class SignificantWhitespaceToken implements Token<MutableRootNode> {
+    @Override
+    public int tryEat(String line, int position, MutableRootNode parsingResult) throws ParseException {
+        int numTabs = leadingTabs(line, position);
+
+        while (numTabs < parsingResult.nestingLevel()) {
+            parsingResult.levelUp();
+        }
+
+        if(numTabs == 0) {
+            return -1;
+        } else {
+            return position + numTabs;
+        }
+    }
+
+    private int leadingTabs(String line, int startPos) {
+        int numTabs = 0;
+        while (line.charAt(startPos + numTabs) == '\t') {
+            numTabs++;
+        }
+        return numTabs;
+    }
+}
