568eebe0dc2d5f3b19d93cb13d2d3cbd8d4250a7
Jan Judas <jan@judas.cz>
2015-04-10 19:53:46 +0200

    inline HtmlTagToken

--- src/main/java/cz/judas/jan/haml/tokens/DocumentToken.java
+++ src/main/java/cz/judas/jan/haml/tokens/DocumentToken.java
@@ -1,7 +1,11 @@
 package cz.judas.jan.haml.tokens;
 
 import cz.judas.jan.haml.ParseException;
+import cz.judas.jan.haml.mutabletree.MutableHtmlNode;
 import cz.judas.jan.haml.mutabletree.MutableRootNode;
+import cz.judas.jan.haml.tokens.generic.ContextSwitchToken;
+import cz.judas.jan.haml.tokens.predicates.IsIdOrClassChar;
+import cz.judas.jan.haml.tokens.predicates.IsTagNameChar;
 
 import static cz.judas.jan.haml.tokens.generic.GenericTokens.*;
 
@@ -13,7 +17,27 @@ public class DocumentToken implements Token<MutableRootNode> {
                                     new DoctypeToken(),
                                     sequence(
                                             new SignificantWhitespaceToken(),
-                                            new HtmlTagToken()
+                                            new ContextSwitchToken<MutableRootNode, MutableHtmlNode>(
+                                                    MutableHtmlNode::new,
+                                                    sequence(
+                                                            atMostOne(
+                                                                    new LeadingCharToken('%', new IsTagNameChar(), MutableHtmlNode::setTagName)
+                                                            ),
+                                                            anyNumberOf(
+                                                                    anyOf(
+                                                                            new LeadingCharToken('.', new IsIdOrClassChar(), MutableHtmlNode::addClass),
+                                                                            new LeadingCharToken('#', new IsIdOrClassChar(), MutableHtmlNode::setId)
+                                                                    )
+                                                            ),
+                                                            anyNumberOf(
+                                                                    new SingleCharToken<MutableHtmlNode>(' ')
+                                                            ),
+                                                            atMostOne(
+                                                                    new TextToken()
+                                                            )
+                                                    ),
+                                                    MutableRootNode::addNode
+                                            )
                                     )
                             ),
                             atMostOne(
