30e50787ccd139d260ff4d5f642e786d8a5fe7a9
Jan Judas <jan@judas.cz>
2015-04-11 17:18:59 +0200

    text lines are not escaped

--- src/test/java/cz/judas/jan/haml/HamlParserTest.java
+++ src/test/java/cz/judas/jan/haml/HamlParserTest.java
@@ -1,15 +1,31 @@
 package cz.judas.jan.haml;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class HamlParserTest {
+
+    private HamlParser parser;
+
+    @Before
+    public void setUp() throws Exception {
+        parser = new HamlParser();
+    }
+
     @Test
     public void processesHaml() throws Exception {
-        HamlParser parser = new HamlParser();
+        assertParses("!!! 5\n%html\n\t%head\n\t\t%title blah", "<!DOCTYPE html>\n<html><head><title>blah</title></head></html>");
+    }
 
-        assertThat(parser.process("!!! 5\n%html\n\t%head\n\t\t%title blah"), is("<!DOCTYPE html>\n<html><head><title>blah</title></head></html>"));
+    @Test
+    public void textLinesAreNotEscaped() throws Exception {
+        assertParses("%p\n\t<div id=\"blah\">Blah!</div>", "<p><div id=\"blah\">Blah!</div></p>");
+    }
+
+    private void assertParses(String input, String output) throws ParseException {
+        assertThat(parser.process(input), is(output));
     }
 }
