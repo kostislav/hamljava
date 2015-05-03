7f1f0cd24c67cd09d8b362eca9e2861b904cca57
Jan Judas <jan@judas.cz>
2015-05-03 17:11:29 +0200

    delete TypedToken

--- src/main/java/cz/judas/jan/haml/parser/tokens/TypedToken.java
+++
@@ -1,9 +0,0 @@
-package cz.judas.jan.haml.parser.tokens;
-
-import cz.judas.jan.haml.parser.InputString;
-
-import java.util.Optional;
-
-public interface TypedToken<C, T> {
-    Optional<T> tryEat(InputString line);
-}
