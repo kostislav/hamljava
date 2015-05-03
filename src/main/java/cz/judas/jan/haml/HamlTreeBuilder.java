f1ccf93b407001f7800cc370bb4e6e861c19e34f
Jan Judas <jan@judas.cz>
2015-05-03 15:41:39 +0200

    delete Parser

--- src/main/java/cz/judas/jan/haml/HamlTreeBuilder.java
+++ src/main/java/cz/judas/jan/haml/HamlTreeBuilder.java
@@ -1,18 +1,24 @@
 package cz.judas.jan.haml;
 
 import cz.judas.jan.haml.grammar.HamlGrammar;
-import cz.judas.jan.haml.parser.Parser;
+import cz.judas.jan.haml.parser.InputString;
+import cz.judas.jan.haml.parser.tokens.TokenCache;
+import cz.judas.jan.haml.parser.tokens.TypedToken;
 import cz.judas.jan.haml.tree.RootNode;
 import cz.judas.jan.haml.tree.mutable.MutableRootNode;
 
 import java.util.Optional;
 
 public class HamlTreeBuilder {
-    private final Parser<MutableRootNode, Optional<String>> parser = new Parser<>(new HamlGrammar());
+    private final TypedToken<MutableRootNode, Optional<String>> token;
+
+    public HamlTreeBuilder() {
+        token = TokenCache.build(new HamlGrammar());
+    }
 
     public RootNode buildTreeFrom(String input) {
         MutableRootNode context = new MutableRootNode();
-        Optional<String> doctype = parser.parse(input, context);
+        Optional<String> doctype = token.tryEat(new InputString(input), context).get();
         doctype.ifPresent(context::setDoctype);
         return context.toNode();
     }
