9c2e89c68448dea9f5e9fc616b537677b678d671
Jan Judas <jan@judas.cz>
2015-05-23 15:06:40 +0200

    replace HamlTreeBuilder with HamlTreeBuilder2

--- src/main/java/cz/judas/jan/haml/HamlParser.java
+++ src/main/java/cz/judas/jan/haml/HamlParser.java
@@ -3,7 +3,7 @@ package cz.judas.jan.haml;
 import cz.judas.jan.haml.tree.RootNode;
 
 public class HamlParser {
-    private final HamlTreeBuilder2 treeBuilder = new HamlTreeBuilder2();
+    private final HamlTreeBuilder treeBuilder = new HamlTreeBuilder();
 
     public String process(String haml, VariableMap variableMap) {
         RootNode rootNode = treeBuilder.buildTreeFrom(haml);
