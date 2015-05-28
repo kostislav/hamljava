27c01519d162af1fa85d78779d39275ee82cc1d6
Jan Judas <jan@judas.cz>
2015-05-28 18:49:49 +0200

    interpolated strings

---
+++ src/main/java/cz/judas/jan/haml/tree/ruby/CompoundStringExpression.java
@@ -0,0 +1,35 @@
+package cz.judas.jan.haml.tree.ruby;
+
+import com.google.common.collect.ImmutableList;
+import cz.judas.jan.haml.HtmlOutput;
+import cz.judas.jan.haml.TemplateContext;
+import cz.judas.jan.haml.ruby.RubyObject;
+import cz.judas.jan.haml.ruby.RubyString;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CompoundStringExpression implements RubyExpression {
+    private final List<RubyExpression> parts;
+
+    public CompoundStringExpression(Iterable<? extends RubyExpression> parts) {
+        this.parts = ImmutableList.copyOf(parts);
+    }
+
+    @Override
+    public RubyObject evaluate(HtmlOutput htmlOutput, TemplateContext templateContext) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (RubyExpression part : parts) {
+            stringBuilder.append(part.evaluate(htmlOutput, templateContext).asString());
+        }
+        return new RubyString(stringBuilder.toString());
+    }
+
+    public static RubyExpression from(Collection<? extends RubyExpression> parts) {
+        if(parts.size() == 1) {
+            return parts.iterator().next();
+        } else {
+            return new CompoundStringExpression(parts);
+        }
+    }
+}
