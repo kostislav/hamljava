a6b6d7f9b9d9dd45774f167d0b81ed17030181b8
Jan Judas <jan@judas.cz>
2015-05-30 21:08:53 +0200

    remove random implementations from Nil

--- src/main/java/cz/judas/jan/haml/ruby/Nil.java
+++ src/main/java/cz/judas/jan/haml/ruby/Nil.java
@@ -1,22 +1,7 @@
 package cz.judas.jan.haml.ruby;
 
-import cz.judas.jan.haml.template.HtmlOutput;
-import cz.judas.jan.haml.template.TemplateContext;
-
-import java.util.List;
-
 public class Nil implements RubyObject {
     public static final Nil INSTANCE = new Nil();
-
-    @Override
-    public RubyObject callMethod(String name, List<RubyObject> arguments, RubyBlock block, HtmlOutput htmlOutput, TemplateContext templateContext) {
-        return INSTANCE;
-    }
-
-    @Override
-    public RubyObject getProperty(String name, HtmlOutput htmlOutput, TemplateContext templateContext) {
-        return INSTANCE;
-    }
 
     @Override
     public String asString() {
