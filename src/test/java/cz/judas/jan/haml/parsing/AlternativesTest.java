f634a8efbf4c8c0228f504b4b751c03786620328
Jan Judas <jan@judas.cz>
2015-05-31 11:50:16 +0200

    AlternativesTest

---
+++ src/test/java/cz/judas/jan/haml/parsing/AlternativesTest.java
@@ -0,0 +1,36 @@
+package cz.judas.jan.haml.parsing;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class AlternativesTest {
+    @Test
+    public void usesFirstIfNotNull() throws Exception {
+        String result = Alternatives
+                .either(6, value -> "x" + value)
+                .or(7, value -> "y" + value)
+                .orException();
+
+        assertThat(result, is("x6"));
+    }
+
+    @Test
+    public void usesSecondIfFirstIsNull() throws Exception {
+        String result = Alternatives
+                .either((Integer)null, value -> "x" + value)
+                .or(7, value -> "y" + value)
+                .orException();
+
+        assertThat(result, is("y7"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionIfAllAreNull() throws Exception {
+        Alternatives
+                .either((Integer)null, value -> "x" + value)
+                .or((Integer)null, value -> "y" + value)
+                .orException();
+    }
+}
