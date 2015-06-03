46ef31ca03cfe5c9c4ed0e16e547f0bce7f552b6
Jan Judas <jan@judas.cz>
2015-06-03 18:57:57 +0200

    do not extend HamlNode in RubyExpression

--- src/main/java/cz/judas/jan/haml/template/tree/ruby/RubyExpression.java
+++ src/main/java/cz/judas/jan/haml/template/tree/ruby/RubyExpression.java
@@ -1,6 +1,8 @@
 package cz.judas.jan.haml.template.tree.ruby;
 
-import cz.judas.jan.haml.template.tree.HamlNode;
+import cz.judas.jan.haml.template.HtmlOutput;
+import cz.judas.jan.haml.template.TemplateContext;
 
-public interface RubyExpression extends HamlNode {
+public interface RubyExpression {
+    Object evaluate(HtmlOutput htmlOutput, TemplateContext templateContext);
 }
