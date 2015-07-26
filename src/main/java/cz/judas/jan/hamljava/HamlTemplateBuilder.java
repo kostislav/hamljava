471ec4b0c424a47468003c56ec62403550fd3734
Jan Judas <jan@judas.cz>
2015-07-26 12:11:59 +0200

    do not wrap IOExceptions

--- src/main/java/cz/judas/jan/hamljava/HamlTemplateBuilder.java
+++ src/main/java/cz/judas/jan/hamljava/HamlTemplateBuilder.java
@@ -10,20 +10,20 @@ import java.nio.charset.Charset;
 public class HamlTemplateBuilder {
     private final HamlTreeBuilder treeBuilder = new HamlTreeBuilder();
 
-    public HamlTemplate buildFrom(Reader input) {
-        try {
-            RootNode rootNode = treeBuilder.buildTreeFrom(input);
-            return new HamlTemplate(rootNode);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not read input", e);
-        }
+    public HamlTemplate buildFrom(Reader input) throws IOException {
+        RootNode rootNode = treeBuilder.buildTreeFrom(input);
+        return new HamlTemplate(rootNode);
     }
 
     public HamlTemplate buildFrom(String input) {
-        return buildFrom(new StringReader(input));
+        try {
+            return buildFrom(new StringReader(input));
+        } catch (IOException e) {
+            throw new RuntimeException("Unexpected IOException when reading from string", e);
+        }
     }
 
-    public HamlTemplate buildFrom(InputStream input, Charset encoding) {
+    public HamlTemplate buildFrom(InputStream input, Charset encoding) throws IOException {
         return buildFrom(new InputStreamReader(input, encoding));
     }
 }
